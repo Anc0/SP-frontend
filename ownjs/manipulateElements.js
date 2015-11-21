a8973acb46230d2cf13593dbec8f014ad8f674c0
Anc0 <ak6688@student.uni-lj.si>
2015-11-21 22:22:29 +0100

    Report submit file completed.

--- ownjs/manipulateElements.js
+++ ownjs/manipulateElements.js
@@ -25,9 +25,45 @@ function showSubjectList(student) {
 function showGradesList(student) {
     var output = document.getElementById("grades-list");
     if(student == 12) {
-        output.innerHTML = "<table id=\"getgrades-table\" class=\"table table-striped\"><thead><tr><th>Predmet</th><th>Prvi semester</th><th>Drugi semester</th><th>Povprečje</th></tr></thead><tbody><tr class=\"success\"><td>Matematika</td><td>5, 3, 3</td><td>4, 2, 3</td><td>3,33</td></tr><tr class=\"success\"><td>Slovenščina</td><td>4, 3, 2</td><td>1, 3, 2</td><td>2,50</td></tr><tr class=\"danger\"><td>Angleščina</td><td>1, 1, 2</td><td>2, 3, 1</td><td>1,67</td></tr></tbody></table>";
+        output.innerHTML = "<table id=\"getgrades-table\" class=\"table table-striped\"><thead><tr><th>Predmet</th><th>Prvi semester</th><th>Drugi semester</th><th>Povprečje</th></tr></thead><tbody><tr class=\"success\"><td>Angleščina</td><td>4, 3, 2</td><td>1, 3, 2</td><td>2,50</td></tr><tr class=\"success\"><td>Fizika</td><td>2, 1, 5</td><td>3, 5, 2</td><td>3,00</td></tr><tr class=\"danger\"><td>Geografija</td><td>1, 1, 2</td><td>2, 3, 1</td><td>1,67</td></tr><tr class=\"success\"><td>Kemija</td><td>5, 2, 2</td><td>2, 5, 5</td><td>3,50</td></tr><tr class=\"success\"><td>Matematika</td><td>5, 3, 3</td><td>4, 2, 3</td><td>3,33</td></tr><tr class=\"success\"><td>Slovenščina</td><td>4, 3, 2</td><td>1, 3, 2</td><td>2,50</td></tr></tbody></table>";
     }
     else {
         output.innerHTML = "Dijak ne obstaja.";
+    }
+}
+
+function showStudentListReport() {
+    var output = document.getElementById("report-student-list");
+    output.innerHTML = "<table class=\"table table-striped table\"><thead><tr><th>Odsoten</th><th>Ime</th><th>Priimek</th></tr></thead><tbody><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Neža</td><td>Avbelj</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Vlatko</td><td>Čančar</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Gregor</td><td>Horvat</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Alja</td><td>Koren</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Miha</td><td>Lapornik</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Miša</td><td>Marinček</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Blaž</td><td>Mesiček</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Mitja</td><td>Nikolič</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Luka</td><td>Rupnik</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Sergeja</td><td>Stefanišin</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"\"></label></div></td><td>Saša</td><td>Zagorac</td></tr><tr><td><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"12\"></label></div></td><td>Ana</td><td>Zrimšek</td></tr></tbody></table>";
+}
+
+function validateReportData() {
+    var output = document.getElementById("report-validity");
+    var sel1 = document.getElementById("select1");
+    var sel2 = document.getElementById("select2");
+    var sel3 = document.getElementById("select3");
+    var sel4 = document.getElementById("select4");
+    var date = document.getElementById("datepicker");
+    if(sel1.selectedIndex == 0) {
+        output.innerHTML = "<div class=\"alert alert-danger\"><strong>Pozor,</strong> niste izbrali letnika.</div>";
+    }
+    else if(sel2.selectedIndex == 0) {
+        output.innerHTML = "<div class=\"alert alert-danger\"><strong>Pozor,</strong> niste izbrali oddelka.</div>";
+    }
+    else if(sel3.selectedIndex == 0) {
+        output.innerHTML = "<div class=\"alert alert-danger\"><strong>Pozor,</strong> niste izbrali predmeta.</div>";
+    }
+    else if(sel4.selectedIndex == 0) {
+        output.innerHTML = "<div class=\"alert alert-danger\"><strong>Pozor,</strong> niste izbrali učilnice.</div>";
+    }
+    else if(date.value === "") {
+        output.innerHTML = "<div class=\"alert alert-danger\"><strong>Pozor,</strong> niste izbrali datuma.</div>";
+    }
+    else if(comment.value === "") {
+        alert("Oddali boste poročilo brez opisa predelane snovi.");
+        output.innerHTML = "<div class=\"alert alert-success\">Uspešno ste oddali poročilo.</div>";
+    }
+    else {
+        output.innerHTML = "<div class=\"alert alert-success\">Uspešno ste oddali poročilo.</div>";
     }
 }
